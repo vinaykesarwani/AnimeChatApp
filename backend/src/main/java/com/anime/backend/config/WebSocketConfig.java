08ae148bf7185b4244ace1ae76ede2757d2b8a25
Vinay Kesarwani <vinay.kesarwani@oracle.com>
2026-04-08 11:39:23 +0530

    Updated WebSocketConfig

--- backend/src/main/java/com/anime/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/anime/backend/config/WebSocketConfig.java
@@ -27,8 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
-                //.withSockJS()
-        ;
+                .withSockJS();
     }
 
     /**
